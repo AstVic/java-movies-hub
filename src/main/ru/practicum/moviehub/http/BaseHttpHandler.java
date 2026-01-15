07dbdc14747096cc301e7680bda748cb74b03843
Виктория Кузовлева <viktoriakuzovleva@MacBook-Air-Viktoria.local>
2026-01-16 00:29:11 +0300

    add changes from lesson

--- src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
+++ src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
@@ -1,7 +1,54 @@
 package ru.practicum.moviehub.http;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public abstract class BaseHttpHandler implements HttpHandler {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
+abstract class BaseHttpHandler implements HttpHandler {
+    protected static final String CT_JSON = "application/json; charset=UTF-8";
+
+    protected void sendJson(HttpExchange ex, int status, String json) throws IOException {
+        byte[] bytes = json.getBytes(StandardCharsets.UTF_8);
+        ex.getResponseHeaders().set("Content-Type", CT_JSON);
+        ex.sendResponseHeaders(status, bytes.length);
+        try (OutputStream os = ex.getResponseBody()) {
+            os.write(bytes);
+        }
+    }
+
+    protected void sendNoContent(HttpExchange ex) throws java.io.IOException {
+        ex.getResponseHeaders().set("Content-Type", CT_JSON);
+        ex.sendResponseHeaders(204, -1);
+
+    }
+
+    public class MoviesHandler extends BaseHttpHandler {
+
+        @Override
+        public void handle(HttpExchange ex) throws IOException {
+            String method = ex.getRequestMethod();
+            if (method.equalsIgnoreCase("GET")) {
+                sendJson(ex, 200, "[]");
+            }
+        }
+    }
+}
+
+class MoviesHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange ex) throws IOException {
+        String method = ex.getRequestMethod();
+        if (method.equalsIgnoreCase("GET")) {
+            byte[] bytes = "[]".getBytes(StandardCharsets.UTF_8);
+            ex.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
+            ex.sendResponseHeaders(200, bytes.length);
+            try (OutputStream os = ex.getResponseBody()) {
+                os.write(bytes);
+            }
+        }
+    }
 }
